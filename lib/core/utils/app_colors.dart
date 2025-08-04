f5eafbf48a758d64089c350a3356981cf4a5ff75
Roqaia Hassan <roqaiahassanahmed@gmail.com>
2025-08-04 23:12:57 +0300

    add di,helpers,networking,utils,add 2 colors

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
 
 abstract class AppColors {
-  }
+  static const lightGray = Color(0xffF5F6F7);
+  static const gray = Color(0xff99A2AB);
+}
--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,4 +1,15 @@
+import 'package:go_router/go_router.dart';
+import 'package:mobile_booking_online_doctor/features/settings/presentation/views/settings_view.dart';
 
 abstract class AppRouter {
- 
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: "/",
+        builder: (context, state) {
+          return SettingsView();
+        },
+      ),
+    ],
+  );
 }
