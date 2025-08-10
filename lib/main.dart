323e59206c825a59cba85a931ee4daca1477ec1c
Hazem Mohamed <hazemhkcyy@gmail.com>
2025-08-10 14:26:56 +0300

    fix: Uncomment setupNotificationsDependencies in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'package:online_booking_with_doctor/core/di/dependency_injection.dart';
+import 'package:online_booking_with_doctor/core/di/dependency_injection.dart';
 import 'package:online_booking_with_doctor/core/routes/app_routes.dart';
 import 'package:online_booking_with_doctor/features/docdoc_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
-  // setupNotificationsDependencies();
+  setupNotificationsDependencies();
   runApp(DocDocApp(appRouter: AppRoutes()));
 }
