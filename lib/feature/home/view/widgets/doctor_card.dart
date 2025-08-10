065ed6e5234524b183970b61b1130a5060dadfaf
ahmedasaber <ahmeda.saber22@gmail.com>
2025-08-10 16:47:34 +0300

    edit doctor card ad

--- lib/feature/home/view/widgets/doctor_card.dart
+++ lib/feature/home/view/widgets/doctor_card.dart
@@ -4,53 +4,70 @@ import 'package:mobile_booking_online_doctor/core/utils/app_colors.dart';
 import 'package:mobile_booking_online_doctor/core/utils/app_style.dart';
 
 class DoctorCard extends StatelessWidget {
-  const DoctorCard({
-    super.key,
-  });
+  const DoctorCard({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
+      height: 183,
       decoration: BoxDecoration(
         borderRadius: BorderRadiusGeometry.circular(24),
         color: AppColors.primaryColor,
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      child: Stack(
         children: [
+          Align(
+            alignment: Alignment.bottomRight,
+            child: SvgPicture.asset('assets/images/background.svg'),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: ClipRRect(
+              borderRadius: BorderRadius.only(bottomRight: Radius.circular(24)),
+              child: Image.asset(
+                'assets/images/doctor-image1.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
           Padding(
-            padding: const EdgeInsets.only(top: 16.0, left: 16.0),
+            padding: const EdgeInsets.only(left: 16.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
                   'Trusted doctor on your schedule 😉',
-                  style: AppStyle.semiBold10.copyWith(color: Colors.white,)
+                  style: AppStyle.semiBold10.copyWith(color: Colors.white),
                 ),
                 const SizedBox(height: 8),
                 Text(
                   'Consult A Doctor\n— Book Today!',
-                  style: AppStyle.bold18.copyWith(color: Colors.white,)
+                  style: AppStyle.bold18.copyWith(color: Colors.white),
                 ),
-                const SizedBox(height: 8),
+                const SizedBox(height: 16),
                 Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     SizedBox(
-                      width: MediaQuery.of(context).size.width * .25,
-                      height: 40,
+                      width: MediaQuery.of(context).size.width * .23,
+                      height: 32,
                       child: Stack(
                         children: [
                           Positioned(
-                            left: 48,
+                            left: 44,
                             child: CircleAvatar(
+                              radius: 15,
                               backgroundImage: AssetImage(
                                 'assets/images/happy-patient3.png',
                               ),
                             ),
                           ),
                           Positioned(
-                            left: 24,
+                            left: 20,
                             child: CircleAvatar(
+                              radius: 15,
                               backgroundImage: AssetImage(
                                 'assets/images/happy-patient2.png',
                               ),
@@ -59,6 +76,7 @@ class DoctorCard extends StatelessWidget {
                           Positioned(
                             left: 0,
                             child: CircleAvatar(
+                              radius: 15,
                               backgroundImage: AssetImage(
                                 'assets/images/happy-patient1.png',
                               ),
@@ -67,44 +85,25 @@ class DoctorCard extends StatelessWidget {
                         ],
                       ),
                     ),
-                    const SizedBox(width: 8),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                         Text(
+                        Text(
                           '30.000+',
-                          style: AppStyle.extraBold12.copyWith(color: Colors.white,)
+                          style: AppStyle.extraBold10.copyWith(
+                            color: Colors.white,
+                          ),
                         ),
                         const SizedBox(width: 8),
                         Text(
                           'Happy Patients',
-                          style: AppStyle.regular12.copyWith(color: Colors.white)
+                          style: AppStyle.regular10.copyWith(
+                            color: Colors.white,
+                          ),
                         ),
                       ],
                     ),
                   ],
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Stack(
-              clipBehavior: Clip.none,
-              children: [
-                SvgPicture.asset('assets/images/background.svg'),
-                Positioned(
-                 bottom: 0,
-                  right: 0,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular(24),
-                    ),
-                    child: Image.asset(
-                      'assets/images/doctor-image1.png',
-                      width: 130,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
                 ),
               ],
             ),
